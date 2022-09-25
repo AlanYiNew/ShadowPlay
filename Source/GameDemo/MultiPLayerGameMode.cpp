a97061d36e27d35d6318ea57572eb15a24c5ca8a
AlanYiNew <245655664@qq.com>
2022-09-25 23:37:49 +0800

    场景转移的时候新Controller的BeginPlay 过早执行或不执行导致不能创建新Character 并 Posses, 明天再查了

--- Source/GameDemo/MultiPLayerGameMode.cpp
+++ Source/GameDemo/MultiPLayerGameMode.cpp
@@ -2,17 +2,83 @@
 
 
 #include "MultiPLayerGameMode.h"
+#include "DefaultPaperCharacter.h"
 
 void AMultiPLayerGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) {
 	Super::InitGame(MapName, Options, ErrorMessage);
 	UE_LOG(LogTemp, Warning, TEXT("Hello"));
+
 	
+	TArray<UObject*> player_start;
+	GetObjectsOfClass(APlayerStart::StaticClass(), player_start);
+	for (auto & iter:player_start) {
+		m_PlayerStartPointsUsed.Push(FPlayerStartInfo(Cast<APlayerStart>(iter), false));
+	}
 }
 
-void AMultiPLayerGameMode::InitGameState() {
+void AMultiPLayerGameMode::InitGameState() 
+{
 	UE_LOG(LogTemp, Warning, TEXT("GG"));
 }
 
-AMultiPLayerGameMode::AMultiPLayerGameMode() {
+AMultiPLayerGameMode::AMultiPLayerGameMode() 
+{
 	
+}
+
+
+void AMultiPLayerGameMode::HandleSeamlessTravelPlayer(AController*& C)
+{	
+	auto PC = Cast<AMyPlayerController>(C);
+	bool v = (PC == nullptr);
+	UE_LOG(LogTemp, Error, TEXT("HandleSeamlessTravelPlayer %d"), v);
+}
+
+APlayerStart* AMultiPLayerGameMode::GetAvailablePlayerStartPoint()
+{
+	for (FPlayerStartInfo& iter : m_PlayerStartPointsUsed) {
+		if (!iter.m_used) {
+			iter.m_used = true;
+			return iter.m_player_start;
+		}
+	}
+	return nullptr;
+}
+
+void AMultiPLayerGameMode::ServerRespawnPlayerController_Implementation(UClass* player_character_class, APlayerController* player_controller)
+{
+	
+	APlayerStart* availablePlayerStart = GetAvailablePlayerStartPoint();
+	if (availablePlayerStart == nullptr) {
+		UE_LOG(LogTemp, Error, TEXT("Failed GetAvailablePlayerStartPoint"));
+		return;
+	}
+
+	
+	APawn* pawn = Cast<APawn>(GetWorld()->SpawnActor<ADefaultPaperCharacter>(player_character_class, availablePlayerStart->GetActorTransform()));
+	if (pawn == nullptr) {
+		UE_LOG(LogTemp, Error, TEXT("Failed spawn actor"));
+		return;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("ServerRespawnPlayerController Possess"));
+	player_controller->Possess(pawn);
+	
+}	
+
+AMyPlayerController* AMultiPLayerGameMode::GetGamePlayPlayerController(int index)
+{
+	if (index >= m_PlayerControllers.Num()) {
+		return nullptr;
+	}
+	
+	return m_PlayerControllers[index];
+}
+
+void AMultiPLayerGameMode::SwapPlayerControllers(APlayerController* OldPC, APlayerController* NewPC)
+{
+	Super::SwapPlayerControllers(OldPC, NewPC);
+	auto PC = Cast<AMyPlayerController>(NewPC);
+	bool v = (PC == nullptr);
+	UE_LOG(LogTemp, Error, TEXT("SwapPlayerControllers %d"), v);
+	m_PlayerControllers.Push(PC);
 }
--- Source/GameDemo/MultiPLayerGameMode.h
+++ Source/GameDemo/MultiPLayerGameMode.h
@@ -4,7 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
+#include "MyPlayerController.h"
+#include <GameFramework/PlayerStart.h>
 #include "MultiPLayerGameMode.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FPlayerStartInfo 
+{
+	GENERATED_BODY()
+	APlayerStart* m_player_start;
+	bool m_used;
+	FPlayerStartInfo(APlayerStart* player_start, bool used):  m_player_start(player_start), m_used(used){}
+	FPlayerStartInfo() : FPlayerStartInfo(nullptr, false) {};
+};
 
 /**
  * 
@@ -13,8 +26,25 @@ UCLASS()
 class GAMEDEMO_API AMultiPLayerGameMode : public AGameMode
 {
 	GENERATED_BODY()
+private:
+	
+	TArray<AMyPlayerController*> m_PlayerControllers;
+	TArray<FPlayerStartInfo> m_PlayerStartPointsUsed;
 public:
 	AMultiPLayerGameMode();
 	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
 	virtual void InitGameState() override;
+
+	UFUNCTION(Server, Replicated, BlueprintCallable, Reliable)
+	void ServerRespawnPlayerController(UClass* player_character_class, APlayerController* player_controller);
+
+	UFUNCTION(BlueprintCallable)
+	AMyPlayerController* GetGamePlayPlayerController(int index);
+
+
+	
+	APlayerStart* GetAvailablePlayerStartPoint();
+protected:
+	virtual void HandleSeamlessTravelPlayer(AController*& C) override;
+	virtual void SwapPlayerControllers(APlayerController* OldPC, APlayerController* NewPC) override;
 };
