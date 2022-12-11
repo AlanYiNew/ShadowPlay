7eb5a9df8437be62020cba2957f9c4783de8cf46
AlanYiNew <245655664@qq.com>
2022-12-11 12:56:15 +0800

    引擎版本修改。TODO选个稳定版本5.03

--- Plugins/SpinePlugin/Intermediate/Build/Win64/UnrealEditor/Inc/SpineEditorPlugin/SpineEditorPlugin.init.gen.cpp
+++ Plugins/SpinePlugin/Intermediate/Build/Win64/UnrealEditor/Inc/SpineEditorPlugin/SpineEditorPlugin.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeSpineEditorPlugin_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000040,
-				0xB84E45E2,
+				0x06C2E64C,
 				0xCA0534BA,
 				METADATA_PARAMS(nullptr, 0)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeSpineEditorPlugin_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_SpineEditorPlugin.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SpineEditorPlugin(Z_Construct_UPackage__Script_SpineEditorPlugin, TEXT("/Script/SpineEditorPlugin"), Z_Registration_Info_UPackage__Script_SpineEditorPlugin, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB84E45E2, 0xCA0534BA));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SpineEditorPlugin(Z_Construct_UPackage__Script_SpineEditorPlugin, TEXT("/Script/SpineEditorPlugin"), Z_Registration_Info_UPackage__Script_SpineEditorPlugin, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x06C2E64C, 0xCA0534BA));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
